fbc7bbad248d30e61f0b4d51d07057f2b5fadff6
Kalp <kalpdalal97@gmail.com>
2021-11-09 18:25:05 -0500

    Whole project commit

---
+++ Deployments/app.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+
+import pickle, os
+
+raw_data_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir,'notebooks','random_forest_regression_model.pkl'))
+
+
+# load the model from disk
+loaded_model=pickle.load(open(raw_data_path, 'rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    pathh = os.path.abspath(os.path.join(os.getcwd(), os.pardir,'Deployments','real_2018.csv'))
+    df=pd.read_csv(pathh)
+    my_prediction=loaded_model.predict(df.iloc[:,:-1].values)
+    my_prediction=my_prediction.tolist()
+    return render_template('result.html',prediction = my_prediction)
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
